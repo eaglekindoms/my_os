4b644665e286538272cd70a5aacfa47180032fe6
eaglekindoms <huyongmao35@outlook.com>
2022-07-01 07:06:12 +0000

    规划目录

--- src/build.sh
+++ src/build.sh
@@ -5,9 +5,9 @@ pwd
 ls
 rm -rf out
 mkdir out
-i686-elf-as boot.s -o out/boot.o
-i686-elf-gcc -c kernel.c -o out/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -T linker.ld -o out/myos.bin -ffreestanding -O2 -nostdlib out/boot.o out/kernel.o -lgcc
+i686-elf-as boot/boot.s -o out/boot.o
+i686-elf-gcc -c kernel/kernel.c -o out/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -T config/linker.ld -o out/myos.bin -ffreestanding -O2 -nostdlib out/boot.o out/kernel.o -lgcc
 
 # 验证内核文件
 if grub-file --is-x86-multiboot out/myos.bin; then
@@ -20,5 +20,8 @@ fi
 rm -rf isodir
 mkdir -p isodir/boot/grub
 cp out/myos.bin isodir/boot/myos.bin
-cp grub.cfg isodir/boot/grub/grub.cfg
+cp config/grub.cfg isodir/boot/grub/grub.cfg
 grub-mkrescue -o myos.iso isodir
+
+# run
+qemu-system-i386 -cdrom myos.iso
